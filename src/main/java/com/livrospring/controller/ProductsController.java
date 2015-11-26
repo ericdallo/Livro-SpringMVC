eea327f278dec878cc70c998b64bd76feda0ed62
Eric Dallo <ericdallo06@hotmail.com>
2015-11-25 22:52:26 -0200

    Validacoes nos campos - Capitulo 5

--- src/main/java/com/livrospring/controller/ProductsController.java
+++ src/main/java/com/livrospring/controller/ProductsController.java
@@ -1,9 +1,13 @@
 package com.livrospring.controller;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,17 +16,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.livrospring.dao.ProductDAO;
 import com.livrospring.model.BookType;
 import com.livrospring.model.Product;
+import com.livrospring.validation.ProductValidator;
 
 @Controller
 @Transactional
 @RequestMapping("/produtos")
 public class ProductsController {
 	
+	
+	/* não necessario pelo @NotBlank
+	@InitBinder
+	public void before(WebDataBinder binder){
+		binder.setValidator(new ProductValidator());
+	}*/
+	
 	@Autowired
 	private ProductDAO productDao;
 
 	@RequestMapping("/form")
-	public ModelAndView form(){
+	public ModelAndView form(Product product){
 		ModelAndView mav = new ModelAndView("/products/form");
 		mav.addObject("types",BookType.values());
 		
@@ -30,11 +42,16 @@ public class ProductsController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String save(Product product, RedirectAttributes redirectAttributes){
+	public ModelAndView save(@Valid Product product, BindingResult bindingResult,RedirectAttributes redirectAttributes){
+		
+		if(bindingResult.hasErrors()){
+			return form(product);
+		}
+		
 		productDao.save(product);
 		
 		redirectAttributes.addFlashAttribute("success","Produto cadastrado com sucesso");
-		return "redirect:produtos";
+		return new ModelAndView("redirect:produtos");
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
--- src/main/java/com/livrospring/model/Product.java
+++ src/main/java/com/livrospring/model/Product.java
@@ -1,6 +1,7 @@
 package com.livrospring.model;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
@@ -9,6 +10,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Entity
 public class Product {
@@ -16,12 +22,17 @@ public class Product {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank
 	private String title;
 	
 	@Lob
+	@NotBlank
 	private String description;
-	
+	@Min(30)
 	private int pages;
+	
+	@DateTimeFormat(iso=ISO.DATE)
+	private Calendar releaseDate;
 	
 	@ElementCollection
 	private List<Price> prices = new ArrayList<Price>();
@@ -56,6 +67,14 @@ public class Product {
 
 	public void setPrices(List<Price> prices) {
 		this.prices = prices;
+	}
+
+	public Calendar getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Calendar releaseDate) {
+		this.releaseDate = releaseDate;
 	}
 
 }
